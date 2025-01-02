8c578c918808d4652c20c6612c31ee69db87303e
Ankit <private.ankit047@gmail.com>
2025-01-02 15:17:12 +0530

    Auto-commit at 1/2/2025, 3:17:12 PM

---
+++ dummy-2025-01-02T09-47-11-977Z.js
@@ -0,0 +1,11 @@
+
+// filepath: /Users/ankit/development/simple-git/repo/Docker/dummy-2025-01-02T09-47-11-977Z.js
+function helloWorld() {
+  console.log('Hello, world!');
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { helloWorld, add };
